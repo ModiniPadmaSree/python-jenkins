104347c7963745a0b22b381f64c42785bd3e8b2d
Modini Padma Sree <modinisree@gmail.com>
2025-12-22 20:31:01 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,9 +1,6 @@
 import unittest
-
-class TestApp(unittest.TestCase):
-
-    def test_sample(self):
+class TestBasic(unittest.TestCase):
+    def test_true(self):
         self.assertTrue(True)
-
 if __name__ == "__main__":
     unittest.main()
