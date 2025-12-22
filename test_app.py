033734492318faaadb44e124c6eb46aed81e05f5
Modini Padma Sree <modinisree@gmail.com>
2025-12-22 20:34:05 +0530

    Add unit test for say_hello function

--- test_app.py
+++ test_app.py
@@ -1,6 +1,8 @@
 import unittest
-class TestBasic(unittest.TestCase):
-    def test_true(self):
-        self.assertTrue(True)
+import app
+class TestApp(unittest.TestCase):
+    def test_say_hello(self):
+        result = app.say_hello()
+        self.assertEqual(result, "Hello from Jenkins CI/CD pipeline")
 if __name__ == "__main__":
     unittest.main()
