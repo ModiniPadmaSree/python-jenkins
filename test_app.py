fac7637ecfcfd3e2969fb9cbd8ee234b419c9f79
Modini Padma Sree <modinisree@gmail.com>
2025-12-22 21:40:19 +0530

    Update test to expect new greeting message

--- test_app.py
+++ test_app.py
@@ -3,6 +3,6 @@ import app
 class TestApp(unittest.TestCase):
     def test_say_hello(self):
         result = app.say_hello()
-        self.assertEqual(result, "Hello from Jenkins CI/CD pipeline")
+        self.assertEqual(result, "Hello from Jenkins CI/CD pipeline with AWS")
 if __name__ == "__main__":
     unittest.main()
