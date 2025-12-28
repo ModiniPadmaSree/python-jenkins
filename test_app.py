50a588c4016aab138661e4276d7e7f0ad57ab2b4
Modini Padma Sree <modinisree@gmail.com>
2025-12-28 22:38:44 +0530

    Update expected output in test_say_hello

--- test_app.py
+++ test_app.py
@@ -3,6 +3,6 @@ import app
 class TestApp(unittest.TestCase):
     def test_say_hello(self):
         result = app.say_hello()
-        self.assertEqual(result, "Hello from Jenkins CI/CD pipeline with AWS")
+        self.assertEqual(result, "Hello from Jenkins CI/CD pipeline")
 if __name__ == "__main__":
     unittest.main()
